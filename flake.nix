c642e03492c60e3d973b286594ca612e27432ad5
Soham S Gumaste <sgumas2@uic.edu>
2022-07-07 15:47:13 -0500

    Working!

--- flake.nix
+++ flake.nix
@@ -4,15 +4,14 @@
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs"; };
 
   outputs = inputs@{ self, nixpkgs, ... }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-      packs = packages.x86_64-linux;
+    let pkgs = import nixpkgs { system = "x86_64-linux"; };
     in {
-      packs.ocs-url =
-        import ./packages/ocs-url.nix { inherit pkgs; };
+      packages.x86_64-linux = {
+        ocs-url = import ./packages/ocs-url.nix { inherit pkgs; };
 
-      packs.realmd = import ./packages/realmd.nix { inherit pkgs; };
+        realmd = import ./packages/realmd.nix { inherit pkgs; };
 
+      };
     };
 }
 
--- packages/realmd.nix
+++ packages/realmd.nix
@@ -1,6 +1,31 @@
-{pkgs, ... }:
+{ pkgs, ... }:
 
 pkgs.stdenv.mkDerivation {
 
-  
+  name = "realmd";
+  version = "0.16.3";
+
+  src = builtins.fetchTarball {
+    url =
+      "http://www.freedesktop.org/software/realmd/releases/realmd-0.16.3.tar.gz";
+    sha256 = "1zknqm7j3dnwkwrpnshc0ya8z0sc4qjs11qa4qnw84wyhx19fp1g";
+  };
+
+  buildInputs = with pkgs; [
+    openldap.dev
+    libkrb5
+    packagekit
+    polkit
+    libxslt
+    intltool
+    glib
+    pkg-config
+    systemd
+  ];
+
+  configureFlags = [
+    "--with-distro=redhat"
+    "--disable-doc"
+    "--with-systemd-unit-dir=$out/lib/systemd"
+  ];
 }
